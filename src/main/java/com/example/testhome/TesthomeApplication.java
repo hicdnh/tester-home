aa40c436ca64a0f654fff8d742f6998ed747a8cd
hicdnh <hicdnh@163.com>
2021-08-28 02:22:02 +0800

    创建springboot项目和vue前端项目

---
+++ src/main/java/com/example/testhome/TesthomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesthomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testhome/TesthomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesthomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
